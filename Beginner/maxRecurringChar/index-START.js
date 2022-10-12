f804dbe7e4c48a82328ccd58eadb7d7224fa20a1
linds0202 <lindsayarainey@gmail.com>
2022-10-12 11:05:10 -0700

    max recurring char

--- Beginner/maxRecurringChar/index-START.js
+++ Beginner/maxRecurringChar/index-START.js
@@ -4,11 +4,51 @@ e.g maxRecurringChar('aabacada') // will return 'a'
 */
 
 
-
+//iterative solution using hash map
 function maxRecurringChar(text) {
-    // Code goes here
+    let charMap = {}
+    let maxCharVal = 0
+    let maxChar = ''
+
+
+    for (let char of text) {
+        if(charMap.hasOwnProperty(char)) {
+            charMap[char]++
+        } else {
+            charMap[char] = 1
+        }
+    }
+
+    for(let char in charMap) {
+        if (charMap[char] > maxCharVal) {
+            maxCharVal = charMap[char]
+            maxChar = char
+        }
+    }
+
+    return maxChar
 }
 
+//solution using ej6 & arrays
+function maxRecurringChar(text) {
+    let charMap = {}
+    let charArr = []
+    let valuesArr = []
+    let maxCharVal = 0
 
+    for (let char of text) {
+        if (charMap.hasOwnProperty(char)) {
+            charMap[char]++
+        } else {
+            charMap[char] = 1
+        }
+    }
+
+    charArr = Object.keys(charMap)
+    valuesArr = Object.values(charMap)
+    maxCharVal = Math.max(...valuesArr)
+
+    return charArr[valuesArr.indexOf(maxCharVal)]
+}
 
 module.exports = maxRecurringChar;
