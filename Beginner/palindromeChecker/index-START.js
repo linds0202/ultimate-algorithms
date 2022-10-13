7e13f49481d794b891fb65edd1a1fda7860d7e07
linds0202 <lindsayarainey@gmail.com>
2022-10-13 10:36:40 -0700

    palindrome checker

--- Beginner/palindromeChecker/index-START.js
+++ Beginner/palindromeChecker/index-START.js
@@ -5,9 +5,30 @@ e.g palindromeChecker('racecar') // will return true
 
 
 
-
+//split / reverse / join
 function palindromeChecker(text) {
-    v// Code goes here
+    return text === text.toLowerCase().split('').reverse().join('')
+}
+
+//split / reverse / join arrow function
+const palindromeChecker = text => text === text.toLowerCase().split('').reverse().join('')
+
+//looping & comparing
+function palindromeChecker(text) {
+    let charArr = text.toLowercase().split('')
+
+    return charArr.every((char, i) => char === charArr[charArr.length - i - 1])
+}
+
+//only check half 
+function palindromeChecker(text) {
+    let textLen = text.length
+    for(let i = 0; i < textLen / 2; i++) {
+        if (text[i] !== text[textLen - i - 1]) {
+            return false
+        }
+    }
+    return true
 }
 
 
