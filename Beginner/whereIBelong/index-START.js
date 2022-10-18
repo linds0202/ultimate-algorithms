f959d146efc64aeeb7bf6c1959c8e4a4d948db29
linds0202 <lindsayarainey@gmail.com>
2022-10-18 09:27:15 -0700

    where I belong

--- Beginner/whereIBelong/index-START.js
+++ Beginner/whereIBelong/index-START.js
@@ -8,12 +8,51 @@ than 1(index 0), but less than 2(index 1).
 */
 
 
-
+//using for loop
 function whereIBelong(arr, num) {
-   // Code goes here
+   arr.sort((a, b) => a - b)
 
+   for(let i = 0; i < arr.length; i++) {
+      if(arr[i] >= num) {
+         return i
+      }
+   }
+
+   return arr.length
 }
 
+//using for loop to find # of lesser values FASTEST BY A LOT
+function whereIBelong(arr, num) {
+   let counter = 0
 
+   arr.sort((a, b) => a - b)
+
+   for (let i = 0; i < arr.length; i++) {
+      if(arr[i] < num) {
+         counter++
+      }
+   }
+
+   return counter
+}
+
+//using a while loop
+function whereIBelong(arr, num) {
+   arr.sort((a, b) => a - b)
+   let counter = 0
+
+   while (arr[counter] < num) {
+      counter++
+   }
+
+   return counter
+}
+
+//using indexOf() & push()
+function whereIBelong(arr, num) {
+   arr.push(num)
+   arr.sort((a, b) => a - b)
+   return arr.indexOf(num)
+}
 
 module.exports = whereIBelong
