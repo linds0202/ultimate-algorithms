621384fca19047e8478a9d3f4332aa9131c3217c
linds0202 <lindsayarainey@gmail.com>
2022-10-18 09:14:39 -0700

    merge arrays

--- Beginner/mergeArrays/index-START.js
+++ Beginner/mergeArrays/index-START.js
@@ -4,6 +4,8 @@
  E.g mergeArrays([1,2,3,3,3], [1,4,5,2]) // should return [1,2,3,4,5]
 */
 
+
+//using set()
 function mergeArrays(...arrays) {
 
     let jointArray = []
@@ -15,6 +17,40 @@ function mergeArrays(...arrays) {
     return [...new Set([...jointArray])]
 
     
+}
+
+//using filter() is FASTEST
+function mergeArrays(...arrays) {
+
+    let jointArray = []
+    
+    arrays.forEach(array => {
+        jointArray = [...jointArray, ...array]
+    });
+
+    const uniqueArr = jointArray.filter((item, index) => jointArray.indexOf(item) === item) 
+
+    return uniqueArr    
+}
+
+//using reduce()
+function mergeArrays(...arrays) {
+
+    let jointArray = []
+    
+    arrays.forEach(array => {
+        jointArray = [...jointArray, ...array]
+    });
+
+    const uniqueArr = jointArray.reduce((newArr, item) => {
+        if(newArr.includes(item)) {
+            return newArr
+        } else {
+            return [...newArr, item]
+        }
+    }, [])
+
+    return uniqueArr
 }
 
 
