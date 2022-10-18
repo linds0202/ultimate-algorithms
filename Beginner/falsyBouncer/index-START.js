bcacabee044f2b2a1e5b9360a3486ddd5915468f
linds0202 <lindsayarainey@gmail.com>
2022-10-18 09:19:15 -0700

    falsy bouncer

--- Beginner/falsyBouncer/index-START.js
+++ Beginner/falsyBouncer/index-START.js
@@ -5,9 +5,22 @@ and return an array of only truthy values.
 E.g  falsyBouncer([1, 0, null, '', 5]) // should return [1,5]
 */
 
-
+//using for...of
 function falsyBouncer(array) {
-    // Code goes here
+    let ans = []
+
+    for(el in array) {
+        if (el) {
+            ans.push(el)
+        }
+    }
+
+    return ans
+}
+
+//using filter
+function falsyBouncer(array) {
+    return array.filter(el => el === true)
 }
 
 
